4c30e2503dfaff0387187cfcd0df37c1ae1a24c4
Josué Merino Calderón <yoshichicharra11@gmail.com>
2025-03-29 12:53:47 -0500

    Change to hashroute to fix the bug

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,17 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import './App.css'; // Asegúrate que esté después de Bootstrap
-import { BrowserRouter } from "react-router-dom";
+import './App.css';
+import { HashRouter } from "react-router-dom"; // <-- Usa HashRouter
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { ThemeProvider } from './pages/themeContext'; // Importa el ThemeProvider
+import { ThemeProvider } from './pages/themeContext';
 import App from "./App";
-// Importa el ThemeProvider
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    {/* Aquí agregamos el `basename` para que funcione correctamente en GitHub Pages */}
-    <BrowserRouter basename="/malla-curricular-espe">
-      <ThemeProvider> {/* Envuelve la aplicación con el ThemeProvider */}
+    <HashRouter> {/* <-- Cambia BrowserRouter por HashRouter */}
+      <ThemeProvider>
         <App />
       </ThemeProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
